4b99d38aa9bf14d42931c19d97cfbc131e5af43e
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-08 11:45:08 +0530

    [Arka]Added: Logic for maximum of three floting point numbers

--- src/main/java/com/bl/maximumgenerics/TestMaximum.java
+++ src/main/java/com/bl/maximumgenerics/TestMaximum.java
@@ -10,10 +10,20 @@ public class TestMaximum {
             max = three;
         return max;
     }
+    public static Float findMax(Float one, Float two, Float three) {
+            Float max = one;
+        if (two.compareTo(max) > 0)
+            max = two;
+        if (three.compareTo(max) > 0)
+            max = three;
+        return max;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to test maximum");
         System.out.println(findMax(7, 9, 10));
+        System.out.println(findMax(7.24f, 2.56f, 7.23f));
+
     }
 
 }
--- src/test/java/com/bl/maximumgenerics/TestMaximumTest.java
+++ src/test/java/com/bl/maximumgenerics/TestMaximumTest.java
@@ -1,6 +1,7 @@
 package com.bl.maximumgenerics;
 
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,24 +9,39 @@ public class TestMaximumTest {
 
     @Test
     public void if_first_num_max_return_first() {
-        TestMaximum t = new TestMaximum();
-        Integer max = t.findMax(10, 8, 9);
+        Integer max = TestMaximum.findMax(10, 8, 9);
         Assert.assertSame(10, max);
+        //Assert.assertThat(max,CoreMatchers.equalTo(10));
     }
 
     @Test
     public void if_second_num_max_return_second() {
-        TestMaximum t = new TestMaximum();
-        Integer max = t.findMax(10, 81, 9);
-        Assert.assertSame(81, max);
+        Integer max = TestMaximum.findMax(8, 10, 9);
+        Assert.assertSame(10, max);
     }
 
     @Test
     public void if_third_num_max_return_third() {
-        TestMaximum t = new TestMaximum();
-        Integer max = t.findMax(10, 8, 99);
+        Integer max = TestMaximum.findMax(10, 8, 99);
         Assert.assertSame(99, max);
     }
 
+    @Test
+    public void if_first_float_max_return_first() {
+        Float max = TestMaximum.findMax(10.12f, 8.34f, 9.98f);
+        Assert.assertThat(max, CoreMatchers.equalTo(10.12F));
+    }
+
+    @Test
+    public void if_second_float_max_return_second() {
+        Float max = TestMaximum.findMax(8.99f, 10.01f, 9.00f);
+        Assert.assertThat(max, CoreMatchers.equalTo(10.01F));
+    }
+
+    @Test
+    public void if_third_float_max_return_third() {
+        Float max = TestMaximum.findMax(8.9998f, 8.9999f, 9.0001f);
+        Assert.assertThat(max, CoreMatchers.equalTo(9.0001F));
+    }
 
 }
